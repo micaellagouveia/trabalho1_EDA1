7c6970739f3d9545aec885d6de447d1040b471bc
Micaella <micaella2212@gmail.com>
2019-11-02 14:05:51 -0300

    Changing fila_lista.c

--- fila_lista.c
+++ fila_lista.c
@@ -3,22 +3,25 @@
 
 #include "fila_lista.h"
 
-
-int cria_fila(){  
+int cria_fila()
+{
     tamanho = 0;
 
     fila = malloc(sizeof(celula));
     fila->proximo = fila;
-    
-    if(fila==NULL) return 0;
+
+    if (fila == NULL)
+        return 0;
 
     return 1;
 }
 
-int insere_fila(int x){
+int insere_fila(int x)
+{
     celula *novo = malloc(sizeof(celula));
 
-    if(novo==NULL) return 0;
+    if (novo == NULL)
+        return 0;
 
     novo->proximo = fila->proximo;
     fila->proximo = novo;
@@ -26,12 +29,14 @@ int insere_fila(int x){
     fila = novo;
     tamanho++;
     u++;
- 
+
     return 1;
 }
 
-int remove_fila(int *x){
-    if(fila_vazia()) return 0;
+int remove_fila(int *x)
+{
+    if (fila_vazia())
+        return 0;
 
     celula *lixo = fila->proximo;
     *x = lixo->dado;
@@ -43,61 +48,63 @@ int remove_fila(int *x){
     return *x;
 }
 
-int fila_cheia() {
+int fila_cheia()
+{
     return 0;
 }
 
-int tamanho_fila(){
+int tamanho_fila()
+{
     return tamanho;
 }
 
-int fila_vazia(){
-    return fila->proximo==fila;
+int fila_vazia()
+{
+    return fila->proximo == fila;
 }
 
-void print_fila(){
+void print_fila()
+{
 
-    if(!u){
+    if (!u)
+    {
         printf("Fila Vazia.\n");
     }
-    else{
+    else
+    {
 
-    printf("\t");
-    for(celula *j = fila->proximo; j != fila; j = j->proximo) printf("-------");
-    printf("-\n");
+        printf("\t");
+        for (celula *j = fila->proximo; j != fila; j = j->proximo)
+            printf("-------");
+        printf("-\n");
 
-    printf("\t");
-    for(celula *j = fila->proximo; j != fila; j = j->proximo) printf("| %03d ", j->dado);
-    printf("|\n");
+        printf("\t");
+        for (celula *j = fila->proximo; j != fila; j = j->proximo)
+            printf("| %03d ", j->dado);
+        printf("|\n");
 
-    printf("\t");
-    for(celula *j = fila->proximo; j != fila; j = j->proximo) printf("-------");
-    printf("-\n");
+        printf("\t");
+        for (celula *j = fila->proximo; j != fila; j = j->proximo)
+            printf("-------");
+        printf("-\n");
 
-    printf("\t");
-    for(celula *j = fila->proximo; j != fila; j = j->proximo) {
-        if (j==fila->proximo)      printf("   p   ");
-        else if (j->proximo==fila) printf("   u   ");
-        else                       printf("       ");
-  
-    }
-    printf("\n\n");
+        printf("\t");
+        for (celula *j = fila->proximo; j != fila; j = j->proximo)
+        {
+            if (j == fila->proximo)
+                printf("   p   ");
+            else if (j->proximo == fila)
+                printf("   u   ");
+            else
+                printf("       ");
+        }
+        printf("\n\n");
     }
     return;
 }
 
-void reinicia() {
-  /*  celula *lixo;
-
-    for (celula *i=fila; i->proximo!=fila; i=i->proximo) {
-        lixo = i;
-        free(lixo);
-    }
-
-    free(fila);
-    tamanho = 0;
-*/
+void reinicia()
+{
     p = 0;
     u = 0;
-
 }
